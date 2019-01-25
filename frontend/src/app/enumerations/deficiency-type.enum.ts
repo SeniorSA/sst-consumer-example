7c57f68e33aa9b269cbd4decf2bcd94e507493d9
SENIOR\Kelvin.Bianchi <kelvin.bianchi@senior.com.br>
2019-01-25 15:39:29 -0200

    #HCMSST-1096

--- frontend/src/app/enumerations/deficiency-type.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum DeficiencyType {
-  PHYSICAL = 'Física',
-  AUDITORY = 'Auditiva',
-  VISUAL = 'Visual',
-  MENTAL = 'Mental',
-  INTELLECTUAL = 'Intelectual',
-  OTHER = 'Outro tipo de deficiência'
-}
--- frontend/src/app/enumerations/operation-type.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum OperationType {
-  INSERT = 'Inserção',
-  UPDATE = 'Atualização',
-  DELETE = 'Exclusão'
-}
