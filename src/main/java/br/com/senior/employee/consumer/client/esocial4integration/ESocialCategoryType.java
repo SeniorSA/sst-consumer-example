cf91889284cacbef904e7760b0b0d592816f193a
evandrodezuanisenior <evandro.dezuani@senior.com.br>
2024-02-26 15:01:03 -0300

    #HCMGDP-9119 - Adequação enumeração eSocialCategoryType

--- src/main/java/br/com/senior/employee/consumer/client/esocial4integration/ESocialCategoryType.java
+++ src/main/java/br/com/senior/employee/consumer/client/esocial4integration/ESocialCategoryType.java
@@ -8,27 +8,27 @@ package br.com.senior.employee.consumer.client.esocial4integration;
  */
 public enum ESocialCategoryType {
     /**
-     * Empregado - Geral, inclusive o empregado público da administração direta ou indireta contratado pela CLT
+     * Empregado - Geral, inclusive o empregado público da administração direta ou indireta contratado pela CLT.
      */
     Category_101,
     /**
-     * Empregado - Trabalhador Rural por Pequeno Prazo da Lei 11.718/2008
+     * Empregado - Trabalhador Rural por Pequeno Prazo da Lei 11.718/2008.
      */
     Category_102,
     /**
-     * Empregado - Aprendiz
+     * Empregado - Aprendiz.
      */
     Category_103,
     /**
-     * Empregado - Doméstico
+     * Empregado - Doméstico.
      */
     Category_104,
     /**
-     * Empregado - contrato a termo firmado nos termos da Lei 9601/98
+     * Empregado - contrato a termo firmado nos termos da Lei 9601/98.
      */
     Category_105,
     /**
-     * Trabalhador Temporário - contrato nos termos da Lei 6019/74
+     * Trabalhador Temporário - contrato por prazo determinado nos termos da Lei 6019/74.
      */
     Category_106,
     /**
@@ -40,53 +40,141 @@ public enum ESocialCategoryType {
      */
     Category_108,
     /**
-     * Empregado - contrato de trabalho intermitente
+     * Empregado - contrato de trabalho intermitente.
      */
     Category_111,
     /**
-     * Trabalhador Avulso - Portuário
+     * Trabalhador Avulso - Portuário.
      */
     Category_201,
     /**
-     * Trabalhador Avulso - Não Portuário
+     * Trabalhador Avulso - Não Portuário.
      */
     Category_202,
     /**
-     * Servidor Público - Titular de Cargo Efetivo, Magistrado, Ministro de Tribunal de Contas, Conselheiro de Tribunal Contas e Membro do Ministério Público
+     * Servidor Público - Titular de Cargo Efetivo, Magistrado, Ministro de Tribunal de Contas, Conselheiro de Tribunal Contas e Membro do Ministério Público.
      */
     Category_301,
     /**
-     * Servidor Público - Ocupante de Cargo exclusivo em comissão
+     * Servidor Público - Ocupante de Cargo exclusivo em comissão.
      */
     Category_302,
     /**
-     * Exercente de mandato eletivo
+     * Agente Político.
      */
     Category_303,
+    /**
+     * Servidor Público indicado para conselho ou órgão representativo, na condição de representante do governo, órgão ou entidade da administração pública.
+     */
+    Category_305,
+    /**
+     * Servidor Público Temporário, sujeito a regime administrativo especial definido em lei própria.
+     */
+    Category_306,
+    /**
+     * Militar efetivo.
+     */
+    Category_307,
+    /**
+     * Conscrito.
+     */
+    Category_308,
+    /**
+     * Agente Público - Outros.
+     */
+    Category_309,
+    /**
+     * Dirigente Sindical - informação prestada pelo Sindicato.
+     */
+    Category_401,
+    /**
+     * Trabalhador cedido - informação prestada pelo Cessionário.
+     */
+    Category_410,
+    /**
+     * Dirigente sindical - Segurado especial
+     */
+    Category_501,
+    /**
+     * Contribuinte individual - Autônomo em geral, exceto se enquadrado em uma das demais categorias de contribuinte individual.
+     */
+    Category_701,
+    /**
+     * Contribuinte individual - Transportador autônomo de passageiros.
+     */
+    Category_711,
+    /**
+     * Contribuinte individual - Transportador autônomo de carga.
+     */
+    Category_712,
+    /**
+     * Contribuinte individual - Diretor não empregado com FGTS.
+     */
+    Category_721,
+    /**
+     * Contribuinte individual - Diretor não empregado sem FGTS.
+     */
+    Category_722,
+    /**
+     * Contribuinte individual - empresários, sócios e membro de conselho de administração ou fiscal.
+     */
+    Category_723,
+    /**
+     * Contribuinte individual - Cooperado que presta serviços a empresa por intermédio de cooperativa de trabalho.
+     */
+    Category_731,
+    /**
+     * Contribuinte individual - Transportador Cooperado que presta serviços por intermédio de cooperativa de trabalho.
+     */
+    Category_734,
+    /**
+     * Contribuinte individual - Cooperado filiado a Cooperativa de Produção.
+     */
+    Category_738,
+    /**
+     * Contribuinte individual - Micro Empreendedor Individual.
+     */
+    Category_741,
+    /**
+     * Contribuinte individual - magistrado classista temporário da Justiça do Trabalho ou da Justiça Eleitoral que seja aposentado de qualquer regime previdenciário.
+     */
+    Category_751,
+    /**
+     * Contribuinte individual - Associado eleito para direção de Cooperativa, associação ou entidade de classe de qualquer natureza ou finalidade, bem como o síndico ou adm., desde que recebam remuneração.
+     */
+    Category_761,
+    /**
+     * Contribuinte individual - Membro de conselho tutelar, nos termos da Lei nº 8.069, de 13 de julho de 1990.
+     */
+    Category_771,
+    /**
+     * Ministro de confissão religiosa ou membro de vida consagrada, de congregação ou de ordem religiosa.
+     */
+    Category_781,
+    /**
+     * Estagiário.
+     */
+    Category_901,
+    /**
+     * Médico Residente.
+     */
+    Category_902,
+    /**
+     * Bolsista, nos termos da lei 8958/1994.
+     */
+    Category_903,
+    /**
+     * Participante de curso de formação, como etapa de concurso público, sem vínculo de emprego/estatutário.
+     */
+    Category_904,
+    /**
+     * Atleta não profissional em formação que receba bolsa.
+     */
+    Category_905,
     /**
      * Servidor público exercente de mandato eletivo, inclusive com exercício de cargo em comissão
      */
     Category_304,
-    /**
-     * Servidor público indicado para conselho ou órgão representativo, na condição de representante do governo, órgão ou entidade da administração pública
-     */
-    Category_305,
-    /**
-     * Servidor público contratado por tempo determinado, sujeito a regime administrativo especial definido em lei própria
-     */
-    Category_306,
-    /**
-     * Militar
-     */
-    Category_307,
-    /**
-     * Conscrito
-     */
-    Category_308,
-    /**
-     * Agente Público - Outros
-     */
-    Category_309,
     /**
      * Servidor público eventual
      */
@@ -103,110 +191,6 @@ public enum ESocialCategoryType {
      * Servidor público exercente de atividade de instrutoria, capacitação, treinamento, curso ou concurso, ou convocado para pareceres técnicos ou depoimentos
      */
     Category_313,
-    /**
-     * Dirigente Sindical - informação prestada pelo Sindicato
-     */
-    Category_401,
-    /**
-     * Trabalhador cedido/exercício em outro órgão/juiz auxiliar - Informação prestada pelo cessionário/destino
-     */
-    Category_410,
-    /**
-     * Contribuinte individual - Autônomo em geral, exceto se enquadrado em uma das demais categorias de contribuinte individual
-     */
-    Category_701,
-    /**
-     * Contribuinte individual - Transportador autônomo de passageiros
-     */
-    Category_711,
-    /**
-     * Contribuinte individual - Transportador autônomo de carga
-     */
-    Category_712,
-    /**
-     * Contribuinte individual - Diretor não empregado com FGTS
-     */
-    Category_721,
-    /**
-     * Contribuinte individual - Diretor não empregado sem FGTS
-     */
-    Category_722,
-    /**
-     * Contribuinte individual - empresários, sócios e membro de conselho de administração ou fiscal
-     */
-    Category_723,
-    /**
-     * Contribuinte individual - Cooperado que presta serviços a empresa por intermédio de cooperativa de trabalho
-     */
-    Category_731,
-    /**
-     * Contribuinte individual - Transportador Cooperado que presta serviços por intermédio de cooperativa de trabalho
-     */
-    Category_734,
-    /**
-     * Contribuinte individual - Cooperado filiado a Cooperativa de Produção
-     */
-    Category_738,
-    /**
-     * Contribuinte individual - Micro Empreendedor Individual
-     */
-    Category_741,
-    /**
-     * Contribuinte individual - magistrado classista temporário da Justiça do Trabalho ou da Justiça Eleitoral que seja aposentado de qualquer regime previdenciário
-     */
-    Category_751,
-    /**
-     * Contribuinte individual - Associado eleito para direção de Cooperativa, associação ou entidade de classe de qualquer natureza ou finalidade, bem como o síndico ou adm., desde que recebam remuneração
-     */
-    Category_761,
-    /**
-     * Contribuinte individual - Membro de conselho tutelar, nos termos da Lei nº 8.069, de 13 de julho de 1990
-     */
-    Category_771,
-    /**
-     * Ministro de confissão religiosa ou membro de vida consagrada, de congregação ou de ordem religiosa
-     */
-    Category_781,
-    /**
-     * Estagiário
-     */
-    Category_901,
-    /**
-     * Médico Residente
-     */
-    Category_902,
-    /**
-     * Bolsista, nos termos da lei 8958/1994
-     */
-    Category_903,
-    /**
-     * Participante de curso de formação, como etapa de concurso público, sem vínculo de emprego/estatutário
-     */
-    Category_904,
-    /**
-     * Atleta não profissional em formação que receba bolsa
-     */
-    Category_905,
-    /**
-     * Beneficiário Ente Público
-     */
-    Category_9995,
-    /**
-     * Beneficiário Ente Público - Somente Cadastro Beneficio
-     */
-    Category_9996,
-    /**
-     * Demitido com data anterior à sucessão (categoria exclusiva Senior)
-     */
-    Category_9997,
-    /**
-     * Não se aplica
-     */
-    Category_9999,
-    /**
-     * Dirigente sindical - Segurado especial
-     */
-    Category_501,
     /**
      * Beneficiário do programa nacional de prestação de serviço civil voluntário
      */
